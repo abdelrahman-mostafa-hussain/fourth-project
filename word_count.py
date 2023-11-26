c6c8cfc4e7557c376b1d22f9e00e5621fecb1670
abdelrahman <eng.abdelrahman.mostafa.hussain@gmail.com>
2023-11-27 00:00:47 +0200

    create function to check the file

--- word_count.py
+++ word_count.py
@@ -1,5 +1,15 @@
-def main():
-    print("this is the .txt file word counter")
+def isExistFile(path):
+    try:
+        file= open(f"{path}","r")
+        print("file is found")
+        return file
+    except Exception as noFileFound:
+        print("file is not exist")
+        return None
 
-if __name__=="__name__":
+def main():
+    path=input("enter the file path: ")
+    file=isExistFile(path)
+
+if __name__=="__main__":
     main()
