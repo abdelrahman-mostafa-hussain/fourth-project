1ccbbafacb3ac59de1143db82068e2aed6e70c54
abdelrahman <eng.abdelrahman.mostafa.hussain@gmail.com>
2023-11-27 01:10:51 +0200

    create function to rid of specal char

--- word_count.py
+++ word_count.py
@@ -1,3 +1,5 @@
+import re
+
 def isExistFile(path):
     try:
         file= open(f"{path}","r")
@@ -10,11 +12,15 @@ def isExistFile(path):
 def readFile(file):
     return file.read()
 
+def removeUnwantChar(content):
+    return re.sub("[^a-zA-Z0-9\s]","",content)
+
 def main():
     path=input("enter the file path: ")
     file=isExistFile(path)
     if file!=None:
         content=readFile(file)
+        
 
 if __name__=="__main__":
     main()
