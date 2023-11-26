c50e56ff6d2fd2efaed52635254274d842c59cd9
abdelrahman <eng.abdelrahman.mostafa.hussain@gmail.com>
2023-11-27 01:37:07 +0200

    little enhance of code

--- word_count.py
+++ word_count.py
@@ -1,6 +1,6 @@
 import re
 
-def isExistFile(path):
+def isExistFile(path:str):
     try:
         file= open(f"{path}","r")
         print("file is found")
@@ -9,10 +9,10 @@ def isExistFile(path):
         print("file is not exist")
         return None
 
-def readFile(file):
+def readFile(file:str):
     return file.read()
 
-def removeUnwantChar(content):
+def removeUnwantChar(content:str):
     return re.sub("[^a-zA-Z0-9\s]","",content)
 
 def countWord(list):
@@ -23,12 +23,12 @@ def main():
     file=isExistFile(path)
     if file!=None:
         content=readFile(file)
-        new_content=removeUnwantChar(content)
-        word_list=new_content.split(" ")
-        count_word=countWord(word_list)
+        new_content:str=removeUnwantChar(content)
+        word_list:list=new_content.split(" ")
+        count_word:int=countWord(word_list)
         print("the number word in this file is",count_word)
     else:
-        print("the number word in this file is 0, becouse the file is not exist")
+        print("the number word is 0, becouse the file is not exist")
 
 if __name__=="__main__":
     main()
